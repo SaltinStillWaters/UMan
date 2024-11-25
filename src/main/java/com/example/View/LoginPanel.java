23bc0c83aedd171bdcc290e81700b769d2f59349
SaltinStillWaters <SaltinStillWaters@gmail.com>
2024-11-25 14:12:20 +0800

    added remember me and sign up here in login

--- src/main/java/com/example/View/LoginPanel.java
+++ src/main/java/com/example/View/LoginPanel.java
@@ -2,10 +2,15 @@ package com.example.View;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.font.TextAttribute;
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -50,11 +55,30 @@ public class LoginPanel extends JPanel {
         submitButton.setPreferredSize(new Dimension(100, 60));
         submitButton.setFont(getFont().deriveFont(21f));
 
+        //Remember me
+        JCheckBox rememberMeCheckBox = new JCheckBox("Remember me");
+        rememberMeCheckBox.setOpaque(false);
+        rememberMeCheckBox.setFocusPainted(false);
+        rememberMeCheckBox.setFont(new Font("Helvetica", Font.PLAIN, 15));
 
+        //Register here
+        JLabel registerJLabel = new JLabel("Sign up here!");
+        registerJLabel.setForeground(Color.blue);
+        registerJLabel.setCursor(new Cursor(Cursor.HAND_CURSOR));
+
+        Font registerFont = new Font("Helvetica", Font.PLAIN, 15);
+        Map<TextAttribute, Object> attributes = new HashMap<>(registerFont.getAttributes());
+        attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
+        registerJLabel.setFont(registerFont.deriveFont(attributes));
+
+        
         this.add(loginPanel);
         this.add(emailTextField);
-        this.add(new SeperatorPanel(0xedfcff, Config.frameDimension.width, 300));
+        this.add(new SeperatorPanel(0xedfcff, Config.frameDimension.width, 10));
         this.add(passwordTextField);
+        this.add(rememberMeCheckBox);
+        this.add(new SeperatorPanel(0xedfcff, Config.frameDimension.width / 4 - 20, 0));
+        this.add(registerJLabel);
         this.add(new SeperatorPanel(0xedfcff, Config.frameDimension.width, 10));
         this.add(submitButton);
         this.setBounds(Config.frameDimension.width / 2, 0, Config.frameDimension.width / 2, Config.frameDimension.height);
